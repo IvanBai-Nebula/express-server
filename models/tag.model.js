874ceb271c26f9603ac3fa631522e91d44b29881
Ivan <tilony@qq.com>
2025-05-11 14:54:55 +0800

    update

--- models/tag.model.js
+++ models/tag.model.js
@@ -15,12 +15,11 @@ module.exports = (sequelize, Sequelize) => {
         unique: true,
         comment: "标签名称",
       },
-      // CreatedAt 字段 Sequelize 会自动添加, UpdatedAt 通常不需要，除非有修改标签名的需求
     },
     {
       tableName: "Tags",
-      timestamps: true, // 只会创建 createdAt，如果需要 updatedAt，确保模型或业务逻辑支持修改标签名
-      updatedAt: false, // 通常标签创建后不修改名称，如果需要修改，则设为true
+      timestamps: true,
+      updatedAt: false,
       comment: "标签表",
       indexes: [
         {
